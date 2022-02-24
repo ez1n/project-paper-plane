60953ba3bea936eac2a3dd8c3a23c8cac606cc7a
Seong Heung Bae <mopil1102@gmail.com>
2022-02-24 23:48:10 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ document.querySelector(".enter-form").addEventListener("keypress", (event) => {
   }
 })
 
-const url = "https://project-paper-plane.herokuapp.com/names";
+// const url = "https://project-paper-plane.herokuapp.com/names";
 //const url = "http://localhost:5000/names"; // local 테스트용
+const url = "http://paperplane.today/names";
 
 
 overlapCheckBtn.addEventListener("click", () => {
